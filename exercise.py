329c6e3b2a4fb40fe8d7e77efbf6953ff44e2804
jcpulliam <92291368+jcpulliam@users.noreply.github.com>
2021-10-18 21:51:18 -0700

    Add files via upload
    first comment "exercise"

---
+++ exercise.py
@@ -0,0 +1,50 @@
+"""New Docstring for Problem Set 2"""
+
+#problem2a
+def prisoners_dilemma():
+	n = 0
+	strategy_player_a = 'Defect'
+	strategy_player_b = 'Cooperate'
+	while:
+		strategy_player_a != strategy_player_b
+		strategy_player_a = input("Enter 'Defect' or 'Cooperate' ")
+		strategy_player_b = input("Enter 'Defect' or Cooperate") 
+		if strategy_player_a == 'Defect' and strategy_player_b == 'Cooperate':
+			print("A: -20, B: 0")
+		#rest of the if statements 
+
+		if strategy_player_a == 'Cooperate' and strategy_player_b == 'Defect'
+			print("A: 0, B: -20")
+		n += 1
+	
+	
+	else:
+		return n 
+
+#problem 2b
+def factorial(n_arg):
+	fact = 1
+	for i in range(1, n_arg+1):
+		fact = fact * 1
+	return fact
+		print(i)
+print(factorial(5))
+
+#Problem 2c
+
+input list = ['', '', '']
+
+def compute_frequency(input_list):
+	return_dict = {}
+	for val in input_list: 
+		print(val)
+		return_dict[val] = input_list.count(val)
+		print(return_dict)
+	return return_dict
+output = compute_frequency([])
+print(output)
+
+compute_frequency(input_list)
+
+ 
+ 
